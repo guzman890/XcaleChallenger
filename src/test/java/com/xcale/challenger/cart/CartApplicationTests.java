a9bc846ad28545fbb688e49cc5c53321c98cf83e
Pablo Guzman <guzmanfernando890@gmail.com>
2023-04-15 09:39:57 -0500

    api functional

---
+++ src/test/java/com/xcale/challenger/cart/CartApplicationTests.java
@@ -0,0 +1,9 @@
+package com.xcale.challenger.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+}
